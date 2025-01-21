0ee0d635b30819c0d5539339b14955b41d165f28
shahadat <shahadat@polygontech.xyz>
2025-01-21 20:44:55 +0600

    dynamic data passing

--- base/urls.py
+++ base/urls.py
@@ -6,7 +6,7 @@ from .import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('room/', views.room, name='room'),
+    path('room/<str:pk>', views.room, name='room'),
     path('about/', views.about, name='about'), 
 ]
 
--- base/views.py
+++ base/views.py
@@ -48,7 +48,7 @@ def home(request):
     context = {'rooms': rooms}
     return render(request, 'base/home.html', context)
 
-def room(request):
+def room(request, pk):
     return render(request, 'base/room.html')
 
 def about(request):
