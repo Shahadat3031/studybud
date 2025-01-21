b858ca4a5ee0af36fb9987c0f97647faa1d80aab
shahadat <shahadat@polygontech.xyz>
2025-01-21 21:35:15 +0600

    db model preparation

--- base/models.py
+++ base/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Room(models.Model):
+    #host
+    #topic
+    title = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    price = models.FloatField(default=0.0)
+    rating = models.FloatField(default=0.0)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
